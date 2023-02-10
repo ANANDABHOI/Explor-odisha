e9a2a09b42b19dbdc1f9269fdc181d5f81232bb0
ANANDABHOI <anandabhoi5582@gmail.com>
2023-02-10 13:21:59 +0530

    It's City List

---
+++ app/src/main/java/com/example/anrt/Angul.java
@@ -0,0 +1,13 @@
+package com.example.anrt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Angul extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_angul);
+    }
+}
---
+++ app/src/main/java/com/example/anrt/sundargarh.java
@@ -0,0 +1,14 @@
+package com.example.anrt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sundargarh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sundargarh);
+    }
+}
